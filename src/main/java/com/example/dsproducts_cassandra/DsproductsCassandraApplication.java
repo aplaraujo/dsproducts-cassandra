8d6a28cba6877b992159ca82da1fea1e439341fd
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-10-24 08:51:45 -0300

    first commit

---
+++ src/main/java/com/example/dsproducts_cassandra/DsproductsCassandraApplication.java
@@ -0,0 +1,13 @@
+package com.example.dsproducts_cassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsproductsCassandraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsproductsCassandraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dsproducts_cassandra/DsproductsCassandraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dsproducts_cassandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsproductsCassandraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
